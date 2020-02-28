d3e38c500049c9ddd79943f4e16b251d8ffa29d9
frank-hn <harfrank2@gmail.com>
2020-02-28 11:12:16 +0200

    combining features from the data set

--- recommend.py
+++ recommend.py
@@ -11,7 +11,17 @@ def get_index_from_title(title):
 
 ##Step 1: Read CVS File
 df = pd.read_csv("movie_dataset.csv")
-print(df.head())
+print(df.columns)
+
+## step2: Select Features
+features = ['keywords', 'cast', 'genres', 'director']
+
+# step3: Create a column in DF which combines all selected features
+
+def combine_features(row):
+    return row['keyword'] +" "+row['cast']+" "+row['genres']+" "+row['director']
+
+
 ##Step 5: Compute the cosine Similarity on the count_matrix
 
 movie_user_likes = "Avatar"
