723201f1b6817134da951a71a4f4845c50bfa436
frank-hn <harfrank2@gmail.com>
2020-02-28 11:10:50 +0200

    initial model setup

---
+++ recommend.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import numpy as np
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+
+def get_title_from_index(index):
+    return df[df.index == index]["title"].values[0]
+
+def get_index_from_title(title):
+    return df[df.title == title]["index"].values[0]
+
+##Step 1: Read CVS File
+df = pd.read_csv("movie_dataset.csv")
+print(df.head())
+##Step 5: Compute the cosine Similarity on the count_matrix
+
+movie_user_likes = "Avatar"
