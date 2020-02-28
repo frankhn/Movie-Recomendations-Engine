8125987d7fcdd7de120f53f026ad43e92440da94
frank-hn <harfrank2@gmail.com>
2020-02-28 11:16:44 +0200

    creating matrix count from the new combined feature

--- recommend.py
+++ recommend.py
@@ -3,13 +3,15 @@ import numpy as np
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 
+
+
 def get_title_from_index(index):
     return df[df.index == index]["title"].values[0]
 
 def get_index_from_title(title):
     return df[df.title == title]["index"].values[0]
 
-##Step 1: Read CVS File
+##Step 1: Read CVS File data frame(df)
 df = pd.read_csv("movie_dataset.csv")
 # print(df.columns)
 
@@ -31,7 +33,9 @@ df['combined_features'] = df.apply(combine_features, axis=1)
 print(df['combined_features'].head())
 
 ##Step 4: Create count matrix from this new combined column
+cv = CountVectorizer()
 
+count_matrix = cv.fit_transform(df['combined_features'])
 
 ##Step 5: Compute the cosine Similarity on the count_matrix
 
